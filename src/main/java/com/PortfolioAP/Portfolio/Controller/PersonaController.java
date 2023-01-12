898829a1c0ff4ea9665b9d8470517c6e19e2589c
unknown <javiersolis921@hotmail.com>
2023-01-12 19:51:10 -0300

    Se agrega link del portfolio

--- src/main/java/com/PortfolioAP/Portfolio/Controller/PersonaController.java
+++ src/main/java/com/PortfolioAP/Portfolio/Controller/PersonaController.java
@@ -21,7 +21,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/personas")
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = {"https://portfoliofrontend-b6024.web.app/"})
 public class PersonaController {
      @Autowired
     ImpPersonaService personaService;
