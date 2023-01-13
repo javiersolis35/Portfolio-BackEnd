58990c2b3e49f68a3ccf44fcd4d2fd801f22c1c4
unknown <javiersolis921@hotmail.com>
2023-01-12 22:29:42 -0300

    se actualiza backend

--- src/main/java/com/PortfolioAP/Portfolio/Controller/PersonaController.java
+++ src/main/java/com/PortfolioAP/Portfolio/Controller/PersonaController.java
@@ -21,7 +21,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/personas")
-@CrossOrigin(origins = {"https://portfoliofrontend-b6024.web.app/"})
+@CrossOrigin(origins = "https://portfoliofrontend-b6024.web.app")
 public class PersonaController {
      @Autowired
     ImpPersonaService personaService;
--- src/main/java/com/PortfolioAP/Portfolio/Security/Controller/AuthController.java
+++ src/main/java/com/PortfolioAP/Portfolio/Security/Controller/AuthController.java
@@ -32,7 +32,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/auth")
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "https://portfoliofrontend-b6024.web.app")
 public class AuthController {
     @Autowired
     PasswordEncoder passwordEncoder;
