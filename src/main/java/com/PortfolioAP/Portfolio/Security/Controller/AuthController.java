ea10b2164d96767a6ad8c60aa7d8efef580cf51b
unknown <javiersolis921@hotmail.com>
2023-01-14 18:49:45 -0300

    se agrega puerto de 8080 para errores de postman

--- src/main/java/com/PortfolioAP/Portfolio/Security/Controller/AuthController.java
+++ src/main/java/com/PortfolioAP/Portfolio/Security/Controller/AuthController.java
@@ -32,7 +32,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/auth")
-@CrossOrigin(origins = {"https://portfoliofrontend-b6024.web.app","http://localhost:4200"})
+@CrossOrigin(origins = {"https://portfoliofrontend-b6024.web.app","http://localhost:8080"})
 public class AuthController {
     @Autowired
     PasswordEncoder passwordEncoder;
