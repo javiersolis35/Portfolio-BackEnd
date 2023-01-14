bebe24d61a275bb6d60c9b586704a643a1f51171
unknown <javiersolis921@hotmail.com>
2023-01-13 23:08:18 -0300

    se actualizo backend

--- src/main/java/com/PortfolioAP/Portfolio/Controller/CEducacion.java
+++ src/main/java/com/PortfolioAP/Portfolio/Controller/CEducacion.java
@@ -22,7 +22,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/educacion")
-@CrossOrigin(origins = "https://portfoliofrontend-b6024.web.app")
+@CrossOrigin(origins = {"https://portfoliofrontend-b6024.web.app"})
 public class CEducacion {
     @Autowired
     Seducacion sEducacion;
