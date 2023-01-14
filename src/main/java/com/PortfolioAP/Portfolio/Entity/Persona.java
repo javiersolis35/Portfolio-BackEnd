5f95861cba46fad4d4d1fe9711bec530094b5cbc
unknown <javiersolis921@hotmail.com>
2023-01-14 00:40:41 -0300

    actualizo backend

--- src/main/java/com/PortfolioAP/Portfolio/Entity/Persona.java
+++ src/main/java/com/PortfolioAP/Portfolio/Entity/Persona.java
@@ -1,6 +1,7 @@
 
 package com.PortfolioAP.Portfolio.Entity;
 
+import java.io.Serializable;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -10,7 +11,7 @@ import javax.validation.constraints.Size;
 
 
 @Entity
-public class Persona {
+public class Persona implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
