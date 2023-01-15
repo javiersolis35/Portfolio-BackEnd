d6d5a8d75d8d1b1b478b370ac86136cab6ab1cfb
Javier Lezcano Solis <95504705+javiersolis35@users.noreply.github.com>
2023-01-15 17:06:22 -0300

    Update PersonaController.java

--- src/main/java/com/PortfolioAP/Portfolio/Controller/PersonaController.java
+++ src/main/java/com/PortfolioAP/Portfolio/Controller/PersonaController.java
@@ -21,7 +21,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/personas")
-@CrossOrigin(origins = {"https://portfoliofrontend-b6024.web.app", "http://localhost:8080"})
+@CrossOrigin(origins = {"https://portfoliofrontend-b6024.web.app/", "http://localhost:8080"})
 public class PersonaController {
      @Autowired
     ImpPersonaService personaService;
