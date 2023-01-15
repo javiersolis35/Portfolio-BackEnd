a5b736ee1030e3aea705349e60e8eeb761921928
unknown <javiersolis921@hotmail.com>
2023-01-15 15:16:02 -0300

    solucion del error de cors

--- src/main/java/com/PortfolioAP/Portfolio/Security/Controller/AuthController.java
+++ src/main/java/com/PortfolioAP/Portfolio/Security/Controller/AuthController.java
@@ -32,7 +32,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/auth")
-@CrossOrigin(origins = {"https://portfoliofrontend-b6024.web.app","http://localhost:8080"})
+@CrossOrigin(origins = {"https://portfoliofrontend-b6024.web.app"})
 public class AuthController {
     @Autowired
     PasswordEncoder passwordEncoder;
