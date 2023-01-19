203ad8c5c72dc83b560563b72b03c70c0593b998
unknown <javiersolis921@hotmail.com>
2023-01-18 22:05:34 -0300

    se cambia

--- src/main/java/com/PortfolioAP/Portfolio/PortfolioApplication.java
+++ src/main/java/com/PortfolioAP/Portfolio/PortfolioApplication.java
@@ -20,7 +20,7 @@ public WebMvcConfigurer corsConfigurer() {
 		return new WebMvcConfigurer() {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**").allowedOrigins("http://localhost:4200");
+				registry.addMapping("/**").allowedOrigins("https://portfoliofrontend-b6024.web.app");
 			}
 		};
 }
