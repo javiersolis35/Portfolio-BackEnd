91576f8ec9ac4b1986a8f82dbede3c6a1dfbc234
unknown <javiersolis921@hotmail.com>
2023-01-19 19:14:56 -0300

    se agrega config CORS.

--- src/main/java/com/PortfolioAP/Portfolio/PortfolioApplication.java
+++ src/main/java/com/PortfolioAP/Portfolio/PortfolioApplication.java
@@ -20,7 +20,7 @@ public class PortfolioApplication {
 	public CorsFilter corsFilter() {
 		CorsConfiguration corsConfiguration = new CorsConfiguration(); 
 		corsConfiguration.setAllowCredentials(true);
-                    corsConfiguration.setAllowedOrigins(Arrays.asList("https://portfoliofrontend-b6024.web.app/"));
+                    corsConfiguration.setAllowedOrigins(Arrays.asList("https://portfoliofrontend-b6024.web.app"));
 		corsConfiguration.setAllowedHeaders(Arrays.asList("Origin", "Access-Control-Allow-Origin", "Content-Type",
 				"Accept", "Authorization", "Origin, Accept", "X-Requested-With",
 				"Access-Control-Request-Method", "Access-Control-Request-Headers"));
