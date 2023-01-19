239cd0212e71db17347f98a23ee4a380c8b2ee63
unknown <javiersolis921@hotmail.com>
2023-01-19 15:33:34 -0300

    se agrega config CORS

--- src/main/java/com/PortfolioAP/Portfolio/Controller/CHys.java
+++ src/main/java/com/PortfolioAP/Portfolio/Controller/CHys.java
@@ -24,7 +24,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin(origins = {"https://portfoliofrontend-b6024.web.app", "http://localhost:4200"})
+@CrossOrigin(origins = {"https://portfoliofrontend-b6024.web.app", "http://localhost:8080"})
 
 
 @RequestMapping("/skill")
--- src/main/java/com/PortfolioAP/Portfolio/PortfolioApplication.java
+++ src/main/java/com/PortfolioAP/Portfolio/PortfolioApplication.java
@@ -2,9 +2,7 @@ package com.PortfolioAP.Portfolio;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
 
 @SpringBootApplication
 public class PortfolioApplication {
@@ -12,16 +10,4 @@ public class PortfolioApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(PortfolioApplication.class, args);
 	}
-
-
-@Bean
-
-public WebMvcConfigurer corsConfigurer() {
-		return new WebMvcConfigurer() {
-			@Override
-			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**").allowedOrigins("https://portfoliofrontend-b6024.web.app");
-			}
-		};
 }
-	}
